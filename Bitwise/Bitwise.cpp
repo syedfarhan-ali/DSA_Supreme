dc82b321627d9b1518f80ea0752e16056f59c78d
syedfarhan-ali <syedfarhanali012005@gmail.com>
2025-01-28 16:25:47 +0530

    added bitwise

---
+++ Bitwise/Bitwise.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a = 5;
+    int b = 10;
+    cout << (a | b) << endl;
+    //HW
+    bool num = 1;
+    cout << ~num << endl;
+    cout << (~num) << endl;
+    cout << ~(num) << endl;
+
+    cout << (5^10) << endl;
+    cout << (5^5) << endl;
+
+    cout << (-100>>1) << endl;
+    return 0;
+}
