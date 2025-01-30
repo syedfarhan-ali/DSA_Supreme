3ed45a4c5243274b39d7c9af8194ee6123a3be8d
syedfarhan-ali <syedfarhanali012005@gmail.com>
2025-01-30 17:47:02 +0530

    solved

--- Functions/Primefrom1ton.cpp
+++ Functions/Primefrom1ton.cpp
@@ -1,2 +1,34 @@
-//Print all prime numbers from 1 to N
 //UNSOLVED
+#include<iostream>
+using namespace std;
+//Print all prime numbers from 1 to N
+bool isPrime(int number){
+    if (number == 1)
+    {
+        return false;
+    }
+    for (int iteration = 2; iteration < number; iteration++)
+    {
+        if (number % iteration == 0)
+        {
+            return false;
+        }
+        
+    }
+    return true;
+    
+}
+int main()
+{
+    int INPUT = 100;
+    for (int count = 1; count <= INPUT; count++)
+    {
+        if (isPrime(count))
+        {
+            cout << count << " ";
+        }
+        
+    }
+    
+    return 0;
+}
