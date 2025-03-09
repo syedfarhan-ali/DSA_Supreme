8abbeec1cf22bc5cd4bad764279a8c890899ce98
syedfarhan-ali <syedfarhanali012005@gmail.com>
2025-03-09 11:55:30 +0530

    updated class2

--- Pointers/class2.cpp
+++ Pointers/class2.cpp
@@ -1,16 +1,22 @@
 #include<iostream>
 using namespace std;
-
+void solve(int arr[],int size){
+    cout << "Size of array in solve : "<< sizeof(arr) << endl;// prints 8
+}
 int main()
 {
-    int arr[5] = {1,2,3,4,5};
-    //pointer to an array
-    int* ptr1 = arr;
-    //pointer to an array
-    int (*ptr)[5] = &arr;
-    cout << (*ptr)[3];
+    int array[] = {10,20,30,40,50};
+    cout << "Size of array in main : "<< sizeof(array) << endl;//prints 20
+    solve(array,5);
     
-    //array of pointers
-    int* arrPtr[5];
+    // int arr[5] = {1,2,3,4,5};
+    // //pointer to an array
+    // int* ptr1 = arr;
+    // //pointer to an array
+    // int (*ptr)[5] = &arr;
+    // cout << (*ptr)[3];
+    
+    // //array of pointers
+    // int* arrPtr[5];
     return 0;
 }
